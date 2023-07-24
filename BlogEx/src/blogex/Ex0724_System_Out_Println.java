a09310d4345f4e09daa948c763efbffaeca1fa28
EnChikai <blood319@naver.com>
2023-07-24 20:44:04 +0900

    2023.07.23 20:43 Upload

--- BlogEx/src/blogex/Ex0724_System_Out_Println.java
+++ BlogEx/src/blogex/Ex0724_System_Out_Println.java
@@ -1,15 +1,21 @@
 package blogex;
 
+import java.util.Scanner;
+
 public class Ex0724_System_Out_Println {
 
 	public static void main(String[] args) {
 
 /*
-1. Console에 "Hello World"를 출력하시오
+1. Scanner를 사용하여 Console에 "Hello World"를 입력 및 출력하시오
  */
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Hello World");
+		System.out.println(sc.nextLine());
+		
+		sc.close();
 		
 	}
 
 }
+
