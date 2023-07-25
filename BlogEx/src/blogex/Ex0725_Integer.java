41bf85650f9f38b767452fbe432f76c2e00d32bd
EnChikai <blood319@naver.com>
2023-07-25 21:52:04 +0900

    2023.07.25 21:51 Upload

--- BlogEx/src/blogex/Ex0725_Integer.java
+++ BlogEx/src/blogex/Ex0725_Integer.java
@@ -4,6 +4,20 @@ public class Ex0725_Integer {
 
 	public static void main(String[] args) {
 
+/*
+ * 
+ * 1. num2, num8, num16을 각각 String으로 변환 후 합계를 구하고 
+ * 합계를 다시금 10진수화하여 보기와 같이 출력하시오.
+ *  
+ * 1001 : 2진수의 문자화
+ * 21 : 8진수의 문자화
+ * 1f : 16진수의 문자화
+ * 문자열의 합 : 1001211f
+ * - - - - - - - -
+ * 268,509,471
+ * 
+ * 
+ */
 		
 		int num2 = 0B1001; //11
 		int num8 = 021; //17
@@ -25,9 +39,10 @@ public class Ex0725_Integer {
 		
 		int decimal10 = Integer.parseInt(sumString, 16);
 
-		System.out.println(String.format("%,d", decimal10));
+		System.out.println(String.format("합계의 10진수화 : " + "%,d", decimal10));
 		
 		
 	}
 
 }
+
