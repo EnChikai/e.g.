ca7583305093c1a72659ea10da218af0671e0e8c
EnChikai <blood319@naver.com>
2023-07-26 21:31:23 +0900

    2023.07.26 21:31 Upload

--- BlogEx/src/blogex/Ex0726_Operator.java
+++ BlogEx/src/blogex/Ex0726_Operator.java
@@ -8,14 +8,12 @@ public class Ex0726_Operator {
 1.	아래의 프로그램을 Run 하지 말고 출력값을 계산하시오
 				
 */
-		
 		int A =10; 
 		int B =8;
 		int C =6;
 		int sum = 0;
 		
 		do {
-			
 			if(A<C) {
 				for(; sum<A; sum++) {
 					for(int i = 0; i<C; i++){
@@ -23,23 +21,17 @@ public class Ex0726_Operator {
 						A--;
 					}
 				}
-			
 			}else {
-				
 				while(A>B) {
 					sum = A+B-C;
 					B++;
 				}
 			}
-			
 			A++;
-			
 		}while(A<B);
-		
 		sum += A;
-		
 		System.out.println(sum);
-		
 	}
 
 }
+
