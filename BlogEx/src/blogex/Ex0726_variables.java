b1555ef8797e311b3b3c6e3fb4b050953db12cbe
En_Chikai <blood319@naver.com>
2023-07-26 17:14:33 +0900

    2023.07.26 17:14 Upload

---
+++ BlogEx/src/blogex/Ex0726_variables.java
@@ -0,0 +1,45 @@
+package blogex;
+
+public class Ex0726_variables {
+
+	public static void main(String[] args) {
+
+/*
+1.	아래의 프로그램을 Run 하지 말고 출력값을 계산하시오
+				
+*/
+		
+		int A =10; 
+		int B =8;
+		int C =6;
+		int sum = 0;
+		
+		do {
+			
+			if(A<C) {
+				for(; sum<A; sum++) {
+					for(int i = 0; i<C; i++){
+						A = C+B;
+						A--;
+					}
+				}
+			
+			}else {
+				
+				while(A>B) {
+					sum = A+B-C;
+					B++;
+				}
+			}
+			
+			A++;
+			
+		}while(A<B);
+		
+		sum += A;
+		
+		System.out.println(sum);
+		
+	}
+
+}
