77b56a37e587f2bf8ef2788e72aa45643919b9cc
En_Chikai <blood319@naver.com>
2023-08-11 16:59:40 +0900

    2023.08.11 17:00 Upload

--- BlogEx/src/blogex/Ex0810.java
+++ BlogEx/src/blogex/Ex0810.java
@@ -1,10 +1,21 @@
 package blogex;
 
+import java.util.Arrays;
+
 public class Ex0810 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] arr = {0,2,0,2,0,2,5,7,6,8,7,1,9,1};
+		Arrays.sort(arr);
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+		
+		
 	}
-
-}
+	
+	
+	
+}	
