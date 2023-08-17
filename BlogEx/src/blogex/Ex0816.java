bd0b5f8444a62411fa800462f957c1cacdb3acc8
EnChikai <blood319@naver.com>
2023-08-18 00:59:22 +0900

    2023.08.18 00:59 Upload

--- BlogEx/src/blogex/Ex0816.java
+++ BlogEx/src/blogex/Ex0816.java
@@ -1,22 +1,46 @@
 package blogex;
 
+import java.util.Scanner;
+
 public class Ex0816 {
 
 	public static void main(String[] args) {
+/*
+1. 빈칸을 채워 보기과 같이 완성하시오
+
+보기:
+오늘 저녁 메뉴는?
+: 카레
+
+저녁 메뉴, 카레로 결정!
+재료가 부족하다
+장보러가자
+예상 재료비 15,000원
+마트까지의 거리50.24M
+서둘러서 장보기 완료!
+사용금액 12,500원
+
+ */
+		Scanner sc = new Scanner(System.in);
 		
-		String name = "Alice";
-		System.out.printf("안녕하세요, %s님", name);		
+		System.out.println("오늘 저녁 메뉴는?");
+		System.out.print(": ");
+		String dinner = sc.nextLine();
+		
+		System.out.println();
+		System.out.printf("저녁 메뉴, %s로 결정!"+"\n", dinner);		
+		System.out.println("재료가 부족하다");
+		
+		System.out.printf("%.5s","장보러가자~!");
 		System.out.println();
 		
-//		System.out.printf("%.2s","안녕하세요");
-//		System.out.println();
-//		System.out.println(String.format("총점 %d점",123));
-//		System.out.printf("%s %d","ABC",456);
-//		System.out.println();
-//		System.out.printf("%9.4f",789.123);
-//		System.out.println();
-//		System.out.println(String.format("%.6f",1.234567890));
-//		System.out.printf("총점 %d점\n",345);
+		System.out.println("예상 재료비 " + String.format("%,d원",15000));
+		System.out.printf("마트까지의 거리"+"%.2fM"+"\n",50.235);
+		System.out.print("서둘러서 장보기 완료!\n");
+		System.out.printf("사용금액 %,d원",12500);
+		
+		sc.close();
 		
 	}
 }
+
