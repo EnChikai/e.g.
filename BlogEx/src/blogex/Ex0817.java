2f671517c88cc345f905c32ba7dabdd38ab850d4
EnChikai <blood319@naver.com>
2023-08-19 16:03:50 +0900

    2023.08.19 16:03 Upload

--- BlogEx/src/blogex/Ex0817.java
+++ BlogEx/src/blogex/Ex0817.java
@@ -4,15 +4,50 @@ public class Ex0817 {
 
 	public static void main(String[] args) {
 		
-		String str = "    가나다라    ";
-		str = str.trim();
-		System.out.println(str);
+/*
+1. 아래 변수를 직접 변경하지 않고 trim(), replace(), replaceAll() 
+함수를 사용하여 보기와 같이 출력하시오.
+
+함수 :
+[str1 = str1:가나다라    ]
+[str2 = str2:A. B. C. D. E. F. G. H. I. J. K. L. M]
+
+
+보기 :
+    str1:가나다라    
+str1:가나다라
+str2:A B C D E F G H I J K L M
+str2:ABCDEFGHIJKLM
+str2:A.C.E.G.I.K.M
+str2:a.c.e.g.i.k.m
+str2:a
+str1:가나다라
+true
+
+ */	
 		
-		str = "A. B. C. D. E. F. G. H. I. J. K. L. M";
-		str = str.replace(".","");
-		System.out.println(str);	
-		str = str.replace(" ","");
-		System.out.println(str);	
+		String str1 = "    str1:가나다라    ";
+		System.out.println(str1);
+		str1 = str1.trim();
+		System.out.println(str1);
+		
+		String str2 = "str2:A. B. C. D. E. F. G. H. I. J. K. L. M";
+		str2 = str2.replace(".","");
+		System.out.println(str2);	
+		str2 = str2.replace(" ","");
+		System.out.println(str2);	
+		str2 = str2.replaceAll("[B,D,F,H,J,L]", ".");
+		System.out.println(str2);	
+		str2 = str2.toLowerCase();
+		System.out.println(str2);	
+		str2 = str2.replaceAll("[.,c,e,g,i,k,m]", "");
+		System.out.println(str2);	
+		str2 = str2.replaceAll("[a]", "가나다라");
+		str2 = str2.replaceAll("[2]", "1");
+		System.out.println(str2);
+		System.out.println(str1.equalsIgnoreCase(str2));
+		
 	}
 	
 }
+
