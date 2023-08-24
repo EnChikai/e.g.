7aaa2ae65c72694aceb99c7af19713465425367b
En_Chikai <blood319@naver.com>
2023-08-24 17:44:20 +0900

    2023.08.24 17:44 Upload

---
+++ BlogEx/src/blogex/Ex0824.java
@@ -0,0 +1,19 @@
+package blogex;
+
+import java.util.Random;
+
+public class Ex0824 {
+
+	public static void main(String[] args) {
+		
+		Random ran = new Random();
+		
+		int[] group1 = new int[3];
+		int[] group2 = new int[3];
+		int[] group3 = new int[3];		
+		int[] group4 = new int[2];
+		
+		
+	}
+
+}
