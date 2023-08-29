3a784e21b789466938e6fad1790fd9a8a35eaacf
EnChikai <blood319@naver.com>
2023-08-29 21:18:18 +0900

    2023.08.09 21:18 Upload

--- BlogEx/src/blogex/Ex0828.java
+++ BlogEx/src/blogex/Ex0828.java
@@ -7,6 +7,55 @@ public class Ex0828 {
 
 	public static void main(String[] args) {
 
+/*
+  
+1. 변의 길이가 다른 다각형 넓이를 구하는 프로그램을 만드시오  
+2. 보기와 같이 출력하시오
+
+보기:
+
+꼭지점의 갯수를 입력해주세요
+ >> ㅁ
+잘못된 입력입니다. 재입력 바랍니다.
+ >> -1
+잘못된 입력입니다. 재입력 바랍니다.
+ >> 5
+1번째 "X" 값을 입력해주세요
+-
+잘못된 입력입니다. 재입력 바랍니다.
+1번째 "X" 값을 입력해주세요
+ㅁ
+잘못된 입력입니다. 재입력 바랍니다.
+1번째 "X" 값을 입력해주세요
+-3
+1번째 "Y" 값을 입력해주세요
+-
+잘못된 입력입니다. 재입력 바랍니다.
+1번째 "X" 값을 입력해주세요
+-3
+1번째 "Y" 값을 입력해주세요
+-2
+2번째 "X" 값을 입력해주세요
+-1
+2번째 "Y" 값을 입력해주세요
+4
+3번째 "X" 값을 입력해주세요
+6
+3번째 "Y" 값을 입력해주세요
+1
+4번째 "X" 값을 입력해주세요
+3
+4번째 "Y" 값을 입력해주세요
+10
+5번째 "X" 값을 입력해주세요
+-4
+5번째 "Y" 값을 입력해주세요
+9
+5각형의 넓이는 60cm² 입니다
+  
+  		
+ */
+		
 		Scanner sc = new Scanner(System.in);
 
 		int xVer = 0;
@@ -17,6 +66,7 @@ public class Ex0828 {
 		int[][] coordinate = null;
 
 		System.out.println("꼭지점의 갯수를 입력해주세요");
+		System.out.println("\"0\"입력시 종료됩니다.");
 		for (int i = 0; i < 1; i++) {
 			try {
 				System.out.print(" >> ");
@@ -65,8 +115,8 @@ public class Ex0828 {
 
 					result += Math.abs(multiply);
 
-					System.out.println(vertex + "각형의 넓이는");
-					System.out.print(result / 2);
+					System.out.print(vertex + "각형의 넓이는 ");
+					System.out.print(result / 2 + "cm² 입니다");
 
 				} else {
 
@@ -85,3 +135,4 @@ public class Ex0828 {
 		sc.close();
 	}
 }
+
---
+++ BlogEx/src/blogex/Ex0829.java
@@ -0,0 +1,10 @@
+package blogex;
+
+public class Ex0829 {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
