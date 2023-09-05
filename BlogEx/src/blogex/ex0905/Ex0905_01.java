e3e5bb7078ec4a44f42578be1961950db174ee2d
EnChikai <blood319@naver.com>
2023-09-05 23:04:15 +0900

    2023.09.05 23:05 Upload

---
+++ BlogEx/src/blogex/ex0905/Ex0905_01.java
@@ -0,0 +1,21 @@
+package blogex.ex0905;
+
+import javax.swing.JFrame;
+
+public class Ex0905_01 {
+
+	public static void main(String[] args) {
+/*
+		  
+1.	label과 Icon을 사용하여 사진파일을 새창으로 출력하는 클래스를 만들고
+	extends JFrame를 적용후 별도의 클래스에서 함수로서 이를 출력하시오
+	(단, 창 크기는 변경 불가능하게 하시오)	
+*/
+		JFrame frame = new Ex0905_2();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(405,340);;
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+
+	}
+}
---
+++ BlogEx/src/blogex/ex0905/Ex0905_2.java
@@ -0,0 +1,27 @@
+package blogex.ex0905;
+
+import java.awt.FlowLayout;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+public class Ex0905_2 extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public Ex0905_2() {
+		super("Image Test용");
+		JLabel label;
+		setLayout(new FlowLayout());
+		setResizable(false);
+		
+		Icon img = new ImageIcon(getClass().getResource("rabbit.png"));
+		label = new JLabel("",img,SwingConstants.CENTER);
+		add(label);
+		
+	}
+
+}
