23747cf36c68e3969f2dc201f931f265d8c3faa8
EnChikai <blood319@naver.com>
2023-09-13 20:58:42 +0900

    2023.09.13 20:58 Upload

--- BlogEx/src/blogex/ex0913/Ex0913_1.java
+++ BlogEx/src/blogex/ex0913/Ex0913_1.java
@@ -5,10 +5,42 @@ import java.util.Random;
 public class Ex0913_1 {
 
 	public static void main(String[] args) {
+
+/*
+1. Random(10000000)을 사용하여 0부터 특정 랜덤값만큼 
+   1씩 증가하거나 감소하는 것을 출력하는 프로그램을 구축하시오.
+   추가적으로 해당 프로그램의 계산이 끝날때 까지의 시간을 보기와
+   같이 출력하시오 
+ 
+보기 : 
+
+	.
+	.
+	.
+	.
+4478791
+4478792
+4478793
+4478794
+4478795
+4478796
+4478797
+4478798
+4478799
+4478800
+4478801
+4478802
+4478803
+4478804
+4478805
+4478806
+걸린 시간 : 14초
+  		
+ */
 		
 		Random random = new Random();	
 		
-		int num = random.nextInt(9790597);
+		int num = random.nextInt(10000000);
 		
 		long before = System.currentTimeMillis();
 		
@@ -30,9 +62,9 @@ public class Ex0913_1 {
 			}
 		}
 		
-		
 		long after = System.currentTimeMillis();
 		
 		System.out.println("걸린 시간 : " + (after-before)/1000 + "초");
 	}
 }
+
