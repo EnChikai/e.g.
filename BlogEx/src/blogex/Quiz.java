2ea2d1351b654c6a68c17f48f49a0321043f853c
En_Chikai <blood319@naver.com>
2023-09-21 19:39:44 +0900

    2023.09.21 19:39 Upload

--- BlogEx/src/blogex/Quiz.java
+++ BlogEx/src/blogex/Quiz.java
@@ -5,10 +5,20 @@ public class Quiz {
 	String a;
 	String b;
 			
-	public String method(String str1, String str2) {
-		this.a = str1;
-		this.b = str2;
+	public String str1(String str1) {
+		return this.a = str1;
+	}
+	
+	public String str2(String str2) {
+		return this.a = str2;
+	}
+	
+	public String method(String t2, String t3) {
+		this.a = t2;
+		this.b = t3;
+		
 		String str3 = "Quiz [a=" + this.a + ", "+ "b=" + this.b + "]";
+		
 		return str3;
 	}
 }
--- BlogEx/src/blogex/Quiz_Ex.java
+++ BlogEx/src/blogex/Quiz_Ex.java
@@ -7,13 +7,13 @@ public class Quiz_Ex {
 
 		Scanner sc = new Scanner(System.in);
 
+		Quiz t1 = new Quiz();
+		
 		System.out.println("t2 값 입력 : ");
-		String t2=sc.nextLine();
+		String t2 = new Quiz().str1(sc.nextLine());
 		
 		System.out.println("t3 값 입력 : ");
-		String t3=sc.nextLine();
-		
-		Quiz t1 = new Quiz();
+		String t3 = new Quiz().str2(sc.nextLine());
 		
 		System.out.println(t1.method(t2,t3));
 		
